ba8b3551d7c4eb6888aa7d94d1baa26ee5b315f9
Jakub Białecki <j.bialecki@wearevirtua.com>
2023-05-30 11:33:27 +0200

    Frontend - Add register page to react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,6 +14,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Register } from "./pages/Register/Register";
 
 export interface IToken {
   access_token: string;
@@ -56,6 +57,10 @@ const router = createBrowserRouter([
 
       return redirect("/");
     },
+  },
+  {
+    element: <Register />,
+    path: "/register",
   },
 ]);
 
